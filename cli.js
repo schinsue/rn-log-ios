d51d87296c9bb884e5ec08548bd25190dd2fb5c7
Sergio Chin-Sue <s.chinsue@gmail.com>
2019-02-06 10:13:49 +0000

    Support for javascript output, seperate native outputs out

--- cli.js
+++ cli.js
@@ -1,50 +1,60 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 const appName = process.argv[2];
 const moment = require("moment");
-const {spawn} = require('child_process');
+const { spawn } = require("child_process");
 
 const args = [
-    'stream',
-    '--predicate', `(processImagePath contains "${appName}") and senderImageUUID == processImageUUID`,
-    '--style', 'json'
+  "stream",
+  "--predicate",
+  `(processImagePath contains "${appName}") and senderImageUUID == processImageUUID`,
+  "--style",
+  "json"
 ];
 
-const lg = spawn('log', args);
+const lg = spawn("log", args);
 
-lg.stdout.on('data', data => {
-    const str = data.toString();
+lg.stdout.on("data", data => {
+  const str = data.toString("utf8");
 
-    // Assumption: { is always at the end of a line, } at the start of line.
-    const m = str.match(/\{$[\s\S]+?^\}/mg);
-    if (m === null) {
-        return;
+  // Assumption: { is always at the end of a line, } at the start of line.
+  const m = str.match(/\{$[\s\S]+?^\}/gm);
+  if (m === null) {
+    return;
+  }
+
+  const all = m.map(str => JSON.parse(str));
+
+  all.forEach(data => {
+    const { timestamp, eventMessage } = data;
+    const time = moment(timestamp).format("H:mm:ss");
+    if (!eventMessage) {
+      console.log(
+        "\x1b[34m" + time + "\x1b[0m",
+        "\x1b[1m" + "==>" + "\x1b[0m",
+        "\x1b[33m" + data + "\x1b[0m"
+      );
+      return;
     }
-
-    const all = m.map(str => JSON.parse(str));
-
-    all.forEach(({timestamp, eventMessage}) => {
-        const time = moment(timestamp).format("H:mm:ss");
-        const pos = eventMessage.indexOf("(ADVICE)");
-        let eMsg = "";
-        if (pos === -1) {
-            eMsg = eventMessage
-            console.log(
-                "\x1b[34m" + time + "\x1b[0m",
-                "\x1b[1m" + "==>" + "\x1b[0m", 
-                "\x1b[33m" + eMsg + "\x1b[0m"
-            )
-        } else {
-            return
-        }
-    });
-
+    const pos = eventMessage.indexOf("(ADVICE)");
+    let eMsg = "";
+    if (pos === -1) {
+      eMsg = eventMessage;
+      console.log(
+        "\x1b[34m" + time + "\x1b[0m",
+        "\x1b[1m" + "==>" + "\x1b[0m",
+        "\x1b[33m" + eMsg + "\x1b[0m"
+      );
+    } else {
+      return;
+    }
+  });
 });
 
-lg.stderr.on('data', data => {
-    console.log(`stderr: ${data}`);
+lg.stderr.on("data", data => {
+  console.log(`stderr: ${data}`);
 });
 
-lg.on('close', code => {
-    console.log(`child process exited with code ${code}`);
+lg.on("close", code => {
+  console.log(`child process exited with code ${code}`);
 });
